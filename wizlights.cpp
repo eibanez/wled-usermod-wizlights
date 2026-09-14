6876d06f59eb53a2fd2fb59de010f4fcb00ebe45
Eduardo Ibanez <edu.ibanez@gmail.com>
2026-09-14 09:41:31 -0500

    Clean up comments and format (#1)
    * Code formatting and comment clean up
    
    * More clean up
    
    millis() rollover should not be an issue because we are unsing unsigned integers.

--- wizlights.cpp
+++ wizlights.cpp
@@ -7,9 +7,6 @@
 
 WiFiUDP UDP;
 
-
-
-
 class WizLightsUsermod : public Usermod {
   
   private:
@@ -24,47 +21,47 @@ class WizLightsUsermod : public Usermod {
     long warmWhite;
     long coldWhite;
 
-    IPAddress lightsIP[MAX_WIZ_LIGHTS];    // Stores Light IP addresses
-    bool      lightsValid[MAX_WIZ_LIGHTS]; // Stores Light IP address validity
+    IPAddress lightsIP[MAX_WIZ_LIGHTS];    // Stores light IP addresses
+    bool      lightsValid[MAX_WIZ_LIGHTS]; // Stores light IP address validity (string is formatted light an IP address)
     uint32_t  colorsSent[MAX_WIZ_LIGHTS];  // Stores last color sent for each light
 
-
-
   public:
-
-
-
-    // Send JSON blob to WiZ Light over UDP
-    // RGB or C/W white
-    // TODO:
-    //   Better utilize WLED existing white mixing logic
+    // Send JSON message to WiZ Light over UDP (RGB or C/W white)
     void wizSendColor(IPAddress ip, uint32_t color, bool gammaCorrect) {
+      // Start UDP packet
       UDP.beginPacket(ip, 38899);
 
-      // If no LED color, turn light off. Note wiz light setting for "Off fade-out" will be applied by the light itself.
+      // If color is black, turn light off
+      //   NOTE: Wiz light setting for "Off fade-out" will be applied by the light itself
       if (color == 0) { 
         UDP.print("{\"method\":\"setPilot\",\"params\":{\"state\":false}}");
 
-      // If color is WHITE, try and use the lights WHITE LEDs instead of mixing RGB LEDs
-      } else if (color == 16777215 && useEnhancedWhite){
+      // If color is white, try and use the lights WHITE LEDs instead of mixing RGB LEDs
+      } else if (color == 16777215 && useEnhancedWhite) {
+        // TODO: Better utilize WLED existing white mixing logic
 
-        // set cold white light only
-        if (coldWhite > 0 && warmWhite == 0){
-          UDP.print("{\"method\":\"setPilot\",\"params\":{\"c\":"); UDP.print(coldWhite) ;UDP.print("}}");}
+        // Set cold white light only
+        if (coldWhite > 0 && warmWhite == 0) {
+          UDP.print("{\"method\":\"setPilot\",\"params\":{\"c\":"); UDP.print(coldWhite) ;UDP.print("}}");
+        }
           
-        // set warm white light only
-        if (warmWhite > 0 && coldWhite == 0){
-          UDP.print("{\"method\":\"setPilot\",\"params\":{\"w\":"); UDP.print(warmWhite) ;UDP.print("}}");}
+        // Set warm white light only
+        if (warmWhite > 0 && coldWhite == 0) {
+          UDP.print("{\"method\":\"setPilot\",\"params\":{\"w\":"); UDP.print(warmWhite) ;UDP.print("}}");
+        }
           
-        // set combination of warm and cold white light
-        if (coldWhite > 0 && warmWhite > 0){
-          UDP.print("{\"method\":\"setPilot\",\"params\":{\"c\":"); UDP.print(coldWhite) ;UDP.print(",\"w\":"); UDP.print(warmWhite); UDP.print("}}");}
+        // Set combination of warm and cold white light
+        if (coldWhite > 0 && warmWhite > 0) {
+          UDP.print("{\"method\":\"setPilot\",\"params\":{\"c\":"); UDP.print(coldWhite) ;UDP.print(",\"w\":"); UDP.print(warmWhite); UDP.print("}}");
+        }
 
       // Send color as RGB  
       } else {
+        // Use gamma color correction, as needed
         uint32_t color2 = color;
         if (gammaCorrect) color2 = gamma32(color2);
-        
+
+        // Send RBG information
         UDP.print("{\"method\":\"setPilot\",\"params\":{\"r\":");
         UDP.print(R(color2));
         UDP.print(",\"g\":");
@@ -74,45 +71,51 @@ class WizLightsUsermod : public Usermod {
         UDP.print("}}");
       }
     
+      // Finish UDP packet
       UDP.endPacket();
     }
 
-    // Override definition so it compiles
+    // Override definition of setup() so it compiles
     void setup() {
       
     }
 
-
-    // TODO: Check millis() rollover
+    // The loop() colors updates the light colors
     void loop() {
-      
       // Make sure we are connected first
       if (!WLED_CONNECTED) return;
 
-      // use color gamma correction if enabled, not in realtime mode with gamma disabled or currently overriding RT mode
+      // Use color gamma correction if enabled, not in realtime mode with gamma disabled or currently overriding RT mode
+      //   NOTE: This condition is the same used inside the WLED code
       bool useGammaCorrection = gammaCorrectCol && !(realtimeMode && arlsDisableGammaCorrection && !realtimeOverride);
       
       unsigned long ellapsedTime = millis() - lastTime;
       if (ellapsedTime > updateInterval) {
+        // Track whether any of the lights were updated in this pass
         bool update = false;
+        
         for (uint8_t i = 0; i < MAX_WIZ_LIGHTS; i++) {
-          if (!lightsValid[i]) { continue; }
+          // Skip lights without a valid IP address
+          if (!lightsValid[i]) continue;
+
+          // Get color for this light
           uint32_t newColor = strip.getPixelColor(i);
-          if (forceUpdate || (newColor != colorsSent[i]) || (ellapsedTime > forceUpdateMinutes*60000)){
+
+          // Update Wiz light color, if necessary
+          if (forceUpdate || (newColor != colorsSent[i]) || (ellapsedTime > forceUpdateMinutes*60000)) {
             wizSendColor(lightsIP[i], newColor, useGammaCorrection);
             colorsSent[i] = newColor;
             update = true;
             delay(sendDelay);
-            }
           }
-        if (update) lastTime = millis();
         }
+        
+        if (update) lastTime = millis();
+      }
     }
 
-
-
-    void addToConfig(JsonObject& root)
-    {
+    // Save configuration parameters
+    void addToConfig(JsonObject& root) {
       JsonObject top = root.createNestedObject("wizLightsUsermod");
       top["Interval (ms)"]                = updateInterval;
       top["Send Delay (ms)"]              = sendDelay;
@@ -127,20 +130,18 @@ class WizLightsUsermod : public Usermod {
       }
     }
 
-
-
-    bool readFromConfig(JsonObject& root)
-    {
+    // Load configuration parameters
+    bool readFromConfig(JsonObject& root) {
       JsonObject top = root["wizLightsUsermod"];
       bool configComplete = !top.isNull();
 
-      configComplete &= getJsonValue(top["Interval (ms)"],                updateInterval,     1000);  // How frequently to update the wiz lights
+      configComplete &= getJsonValue(top["Interval (ms)"],                updateInterval,     1000);  // How frequently to update the Wiz lights
       configComplete &= getJsonValue(top["Send Delay (ms)"],              sendDelay,          0);     // Optional delay after sending each UDP message
-      configComplete &= getJsonValue(top["Use Enhanced White *"],         useEnhancedWhite,   false); // When color is white use wiz white LEDs instead of mixing RGB
-      configComplete &= getJsonValue(top["* Warm White Value (0-255)"],   warmWhite,          0);     // Warm White LED value for Enhanced White
-      configComplete &= getJsonValue(top["* Cold White Value (0-255)"],   coldWhite,          50);   // Cold White LED value for Enhanced White
-      configComplete &= getJsonValue(top["Always Force Update"],          forceUpdate,        false); // Update wiz light every loop, even if color value has not changed
-      configComplete &= getJsonValue(top["Force Update Every x Minutes"], forceUpdateMinutes, 5);     // Update wiz light if color value has not changed, every x minutes
+      configComplete &= getJsonValue(top["Use Enhanced White *"],         useEnhancedWhite,   false); // When color is white, use Wiz white LEDs instead of mixing RGB
+      configComplete &= getJsonValue(top["* Warm White Value (0-255)"],   warmWhite,          0);     // Warm white LED value for enhanced white
+      configComplete &= getJsonValue(top["* Cold White Value (0-255)"],   coldWhite,          50);    // Cold white LED value for enhanced white
+      configComplete &= getJsonValue(top["Always Force Update"],          forceUpdate,        false); // Update Wiz lights every loop, even if color value has not changed
+      configComplete &= getJsonValue(top["Force Update Every x Minutes"], forceUpdateMinutes, 5);     // Update Wiz lights if color value has not changed, every x minutes
       
       // Read list of IPs
       String tempIp;
@@ -149,19 +150,21 @@ class WizLightsUsermod : public Usermod {
         lightsValid[i] = lightsIP[i].fromString(tempIp);
         
         // If the IP is not valid, force the value to be empty
-        if (!lightsValid[i]){lightsIP[i].fromString("0.0.0.0");}
-        }
+        if (!lightsValid[i]) lightsIP[i].fromString("0.0.0.0");
+      }
 
       return configComplete;
     }
 
-
-   // Create label for the usermod page (I cannot make it work with JSON arrays...)
-    String getJsonLabel(uint8_t i) {return "WiZ Light IP #" + String(i+1);}
-        
-    uint16_t getId(){return USERMOD_ID_WIZLIGHTS;}
+    // Create label for the usermod HTML page
+    String getJsonLabel(uint8_t i) {
+      return "WiZ Light IP #" + String(i+1);
+    }
+    
+    uint16_t getId() {
+      return USERMOD_ID_WIZLIGHTS;
+    }
 };
-
 
 static WizLightsUsermod wizlights;
 REGISTER_USERMOD(wizlights);
